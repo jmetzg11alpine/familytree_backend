ebc2336deb1806e7a6d49cf05843d0abbab8d508
jmetzg11alpine <jmetzg11@gmail.com>
2024-07-08 11:22:58 -0700

    updated budget data gathering

--- src/initialize_database/budget.py
+++ src/initialize_database/budget.py
@@ -1,49 +1,12 @@
 import os
 import sys
+import csv
 import requests
 from collections import defaultdict
 import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.budget.models import AgencyBudget, FunctionSpending
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-
-def update_budget(session):
-    try:
-        agency_budgets, agency_codes = fetch_budget_resources()
-        function_spending = fetch_function_spending(agency_codes)
-
-        if check_data_quality(agency_budgets, function_spending):
-            clear_previous_data(session)
-
-            add_agency_budgets(agency_budgets, session)
-            add_function_spending(function_spending, session)
-            update_date_in_text_file()
-
-        else:
-            print('data quality for budget or function spending did not pass')
-    except Exception as e:
-        print(f'Could not update Budget tables: {e}')
-
-
-def fetch_budget_resources():
-    agency_codes = fetch_agency_codes()
-
-    agency_budgets = {}
-    for name, code in agency_codes.items():
-        try:
-            print(f'budget resource for {name}')
-            response = requests.get(f'https://api.usaspending.gov/api/v2/agency/{code}/budgetary_resources/')
-            data = response.json()
-            # response layout: https://api.usaspending.gov/api/v2/agency/012/budgetary_resources/
-            # the index 0 has the most recent year
-            budget = data['agency_data_by_year'][0]['agency_budgetary_resources']
-            if budget:
-                agency_budgets[name] = budget
-        except Exception as e:
-            print(f'could not find budget resource for {name}, error: {e}')
-
-    return agency_budgets, agency_codes
 
 
 def fetch_agency_codes():
@@ -63,13 +26,56 @@ def fetch_agency_codes():
 
         page += 1
 
-    return agency_codes
+    file_name = os.path.join(current_dir, '..', 'budget', 'data', 'agency_codes.csv')
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Agency Name', 'Agency Code'])
+        for agency_name, agency_code in agency_codes.items():
+            writer.writerow([agency_name, agency_code])
 
 
-def fetch_function_spending(agency_codes):
+def fetch_budget_resources():
+    agency_codes = {}
+    agency_file_name = os.path.join(current_dir, '..', 'budget', 'data', 'agency_codes.csv')
+    with open(agency_file_name, mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            agency_codes[row['Agency Name']] = row['Agency Code']
+
+    agency_budgets = {}
+    for name, code in agency_codes.items():
+        try:
+            print(f'budget resource for {name}')
+            response = requests.get(f'https://api.usaspending.gov/api/v2/agency/{code}/budgetary_resources/')
+            data = response.json()
+            # response layout: https://api.usaspending.gov/api/v2/agency/012/budgetary_resources/
+            # the index 0 has the most recent year
+            budget = data['agency_data_by_year'][0]['agency_budgetary_resources']
+            if budget:
+                agency_budgets[name] = budget
+        except Exception as e:
+            print(f'could not find budget resource for {name}, error: {e}')
+
+    budget_file_name = os.path.join(current_dir, '..', 'budget', 'data', 'agency_resources.csv')
+    with open(budget_file_name, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Agency', 'Budget'])
+        for agency, budget in agency_budgets.items():
+            writer.writerow([agency, budget])
+
+
+def fetch_function_spending(years_to_check):
     functions_to_keep = ['Energy', 'Net Interest', 'Commerce and Housing Credit', 'Transportation', 'Agriculture', 'Health', 'Education, Training, Employment, and Social Services', 'National Defense']
-    function_spending = defaultdict(lambda: defaultdict(int))
-    for year in range(2017, 2025):
+
+    agency_codes = {}
+    agency_file_name = os.path.join(current_dir, '..', 'budget', 'data', 'agency_codes.csv')
+    with open(agency_file_name, mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            agency_codes[row['Agency Name']] = row['Agency Code']
+
+    for year in years_to_check:
+        function_spending = defaultdict(int)
         for agency_name, agency_code in agency_codes.items():
             try:
                 print(year, agency_name)
@@ -82,40 +88,39 @@ def fetch_function_spending(agency_codes):
                     function_name = result['name']
                     if function_name in functions_to_keep:
                         amount = result['gross_outlay_amount']
-                        function_spending[year][function_name] += amount
+                        function_spending[function_name] += amount
             except Exception as e:
                 print(f'could not find for budget functions for {agency_name}, {e}')
-    return function_spending
+        function_file_name = os.path.join(current_dir, '..', 'budget', 'data', f'functions_{year}.csv')
+        with open(function_file_name, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Function', 'Amount'])
+            for function, amount in function_spending.items():
+                writer.writerow([function, amount])
 
 
-def add_agency_budgets(agency_budgets, session):
-    for agency, budget in agency_budgets.items():
-        agency_budget = AgencyBudget(agency=agency, budget=budget)
-        session.add(agency_budget)
+def add_agency_budgets(session):
+    agency_budgets = {}
+    agency_file_name = os.path.join(current_dir, '..', 'budget', 'data', 'agency_resources.csv')
+    with open(agency_file_name, mode='r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            agency_budget = AgencyBudget(agency=row['Agency'], budget=row['Budget'])
+            session.add(agency_budget)
     session.commit()
-    print('updated agency_budget table')
+    print('agency budget table updated')
 
 
-def add_function_spending(function_spending, session):
-    for year, info in function_spending.items():
-        for name, amount in info.items():
-            function_entry = FunctionSpending(year=year, name=name, amount=amount)
-            session.add(function_entry)
+def add_function_spending(session):
+    for year in range(2017, 2025):
+        function_file_name = os.path.join(current_dir, '..', 'budget', 'data', f'functions_{year}.csv')
+        with open(function_file_name, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                function_entry = FunctionSpending(year=year, name=row['Function'], amount=row['Amount'])
+                session.add(function_entry)
     session.commit()
-    print('updated function_spending table')
-
-
-def check_data_quality(agency_budgets, function_spending):
-    agency = len(agency_budgets) > 16 and sum([amount for agency, amount in agency_budgets.items()]) > 1192291315.25
-    function = len(function_spending) > 5 and sum([item['Net Interest'] for key, item in function_spending.items()]) > 480144186.311
-    return agency and function
-
-
-def clear_previous_data(session):
-    session.query(AgencyBudget).delete()
-    session.query(FunctionSpending).delete()
-    session.commit()
-    print('old tables deleted')
+    print('function spending table updated')
 
 
 def update_date_in_text_file():
@@ -123,3 +128,21 @@ def update_date_in_text_file():
     today = str(datetime.date.today())
     with open(file_name, 'w') as file:
         file.write(today)
+
+
+def update_budget(session):
+    # updates the csv files
+
+    # all the unique agency codes for the government
+    # fetch_agency_codes()
+
+    # budget resources for the most recent year
+    # fetch_budget_resources()
+
+    # get historical function spending (2017, 2025)
+    # years_to_check = [2024]
+    # fetch_function_spending(years_to_check)
+
+    add_agency_budgets(session)
+    add_function_spending(session)
+    update_date_in_text_file()
